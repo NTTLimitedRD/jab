ba43d538911bc1167d1ccd1da852380dac378ea4
Anthony Shaw <anthony.p.shaw@gmail.com>
2016-08-15 14:59:43 +1000

    Added swagger loader and operation enumerator

---
+++ jab/jab/TestExample.cs
@@ -0,0 +1,14 @@
+﻿using NSwag;
+using Xunit;
+
+namespace jab
+{
+    class TestExample
+    {
+        [Theory, ClassData(typeof(ApiOperations))]
+        public void ExampleCase(string path, SwaggerOperationMethod method, SwaggerOperation operation)
+        {
+            Assert.Equal("blue", "green");
+        }
+    }
+}
