23fe112174a07e4c7d4ad0ee39cbb01aa7b0ddd1
jason.dai <jason.dai@didata.com.au>
2016-08-15 15:44:38 +1000

    Created initial webapi project

---
+++ jab/WebApi/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+	public class UsersController : ApiController
+	{
+		public IHttpActionResult GetAllUsers()
+		{
+			return Ok("hello");
+		}
+	}
+}
