60ee23bb8c78a2f75a810c5f59c2db432c6e8ee5
Anthony Langsworth <anthony.langsworth@itaas.dimensiondata.com>
2016-08-16 11:50:27 +1000

    Remove superseded api keys in query parameters test.

--- jab/jab/Test/ParameterTests.cs
+++ jab/jab/Test/ParameterTests.cs
@@ -49,32 +49,9 @@ namespace jab.tests
         }
 
         /// <summary>
-        /// You should not ask for api keys in query parameters. 
-        /// https://www.owasp.org/index.php/REST_Security_Cheat_Sheet#Authentication_and_session_management
-        /// </summary>
-        /// <param name="service"></param>
-        /// <param name="path"></param>
-        /// <param name="method"></param>
-        /// <param name="operation"></param>
-        [Theory, ParameterisedClassData(typeof(ApiOperations), testDefinition)]
-        public void NoApiKeysInQueryParameters(
-            IJabApiOperation operation)
-        {
-            if (operation.Operation.ActualParameters.Count > 0)
-            {
-                Assert.False(
-                    operation.Operation.Parameters.Count(
-                        c => ((c.Name.ToLower() == "apikey") || (c.Name.ToLower() == "api_key")) 
-                            && c.Kind == SwaggerParameterKind.Query) > 0);
-            }
-            else
-            {
-                Assert.True(true);
-            }
-        }
-
-        /// <summary>
         /// Do not include secrets in query parameters. These get logged or included in browser history.
+        /// <para></para>
+        /// Similar to https://www.owasp.org/index.php/REST_Security_Cheat_Sheet#Authentication_and_session_management.
         /// </summary>
         /// <param name="operation"></param>
         [Theory, ParameterisedClassData(typeof(ApiOperations), testDefinition)]
