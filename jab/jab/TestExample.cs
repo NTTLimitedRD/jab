bc67a80a51ef1a726d7292a424854479238b7358
Anthony Shaw <anthony.p.shaw@gmail.com>
2016-08-16 10:01:19 +1000

    Add a check for api keys in query parameters.

--- jab/jab/TestExample.cs
+++ jab/jab/TestExample.cs
@@ -5,9 +5,11 @@ using System.Linq;
 
 namespace jab
 {
-    public class TestExample
+    public partial class TestExample
     {
-        [Theory, ParameterisedClassData(typeof(ApiOperations), "samples/example.json")]
+        const string testDefinition = "samples/example.json";
+
+        [Theory, ParameterisedClassData(typeof(ApiOperations), testDefinition)]
         public void DeleteMethodsShouldNotTakeFormEncodedData(
             SwaggerService service,
             string path, 
@@ -18,6 +20,34 @@ namespace jab
             {
                 Assert.Null(operation.ActualConsumes);
             } else
+            {
+                Assert.True(true);
+            }
+        }
+
+        /// <summary>
+        /// You should not ask for api keys in query parameters. 
+        /// https://www.owasp.org/index.php/REST_Security_Cheat_Sheet#Authentication_and_session_management
+        /// </summary>
+        /// <param name="service"></param>
+        /// <param name="path"></param>
+        /// <param name="method"></param>
+        /// <param name="operation"></param>
+        [Theory, ParameterisedClassData(typeof(ApiOperations), testDefinition)]
+        public void NoApiKeysInParameters(
+            SwaggerService service,
+            string path,
+            SwaggerOperationMethod method,
+            SwaggerOperation operation)
+        {
+            if (operation.ActualParameters.Count > 0)
+            {
+                Assert.False(
+                    operation.Parameters.Count(
+                        c => ((c.Name.ToLower() == "apikey") || (c.Name.ToLower() == "api_key")) 
+                            && c.Kind == SwaggerParameterKind.Query) > 0);
+            }
+            else
             {
                 Assert.True(true);
             }
