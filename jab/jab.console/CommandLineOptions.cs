f7a1828fe526077d40873171df7054d30b0e836b
Anthony Langsworth <anthony.langsworth@itaas.dimensiondata.com>
2016-08-19 17:30:00 +1000

    Add CommandLineParser support.

---
+++ jab/jab.console/CommandLineOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace jab.console
+{
+    public class CommandLineOptions
+    {
+        [ValueOption(0)]
+        public string SwaggerFilePath { get; set; }
+
+        [Option('u', "baseUrl", DefaultValue = null, Required = false)]
+        public string BaseUrl { get; set; }
+    }
+}
--- jab/jab.console/Program.cs
+++ jab/jab.console/Program.cs
@@ -2,13 +2,10 @@
 using System.CodeDom;
 using System.Threading;
 using NUnit.Engine;
-using NUnit.Common;
 using NUnit.Engine.Runners;
 using System.IO;
-using System.Reflection;
 using Autofac;
-using Autofac.Core.Lifetime;
-using jab.Interfaces;
+using CommandLine;
 using jab.tests;
 
 namespace jab.console
@@ -34,39 +31,28 @@ namespace jab.console
         /// </returns>
         public static int Main(string[] args)
         {
-            //if (args.Length == 0 || args.Length > 2)
-            //{
-            //    Console.WriteLine("usage: jab.console.exe <path to swagger.json> [api url]");
-            //    return 2;
-            //}
-
-            //if (args.Length > 1)
-            //{
-            //    Environment.SetEnvironmentVariable(EnvironmentVariables.ActiveTestsFlag, "1", EnvironmentVariableTarget.Process);
-            //    Environment.SetEnvironmentVariable(EnvironmentVariables.BaseUrl, args[1], EnvironmentVariableTarget.Process);
-            //}
-
-            //string fixturesPath = Path.Combine(thisPath, "fixtures");
-
-            //// does the fixtures folder exist?
-            //if (!Directory.Exists(fixturesPath))
-            //    Directory.CreateDirectory(fixturesPath);
-
-            //// Copy the given fixture across
-            //File.Copy(args[0], Path.Combine(fixturesPath, "swagger.json"), true);
-
+            CommandLineOptions commandLineOptions;
             TestEventListener testEventListener;
+            ContainerBuilder containerBuilder;
+
+            commandLineOptions = new CommandLineOptions();
+            if (!Parser.Default.ParseArguments(args, commandLineOptions))
+            {
+                Console.Error.WriteLine("usage: jab.console.exe <path to swagger.json> [api url]");
+                return ExitCodes.BadArgument;
+            }
+
+            // TODO: Better command line error validation
 
             testEventListener = new TestEventListener();
             testEventListener.OnTestCaseResult += TestEventListener_OnTestCaseResult;
             FailedTestCount = 0;
 
-
-            ContainerBuilder containerBuilder;
-
             containerBuilder = new ContainerBuilder();
-            //JabTestConfiguration.Build(containerBuilder, File.ReadAllText(args[0]), new Uri(args[1]));
-            JabTestConfiguration.Register(containerBuilder, File.ReadAllText("example.json"), null);
+            JabTestConfiguration.Register(
+                containerBuilder, 
+                File.ReadAllText(commandLineOptions.SwaggerFilePath),
+                commandLineOptions.BaseUrl != null ? new Uri(commandLineOptions.BaseUrl) : null);
 
             using (IContainer container = containerBuilder.Build())
             using (ITestEngine engine = TestEngineActivator.CreateInstance())
